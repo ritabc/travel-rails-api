5592d3eeee6e6b3f411aad1141a2ad9dbc327c23
Student <example@example.com>
2018-09-24 11:37:51 -0700

    Add resources to routes, add methods to destinationscontroller
    Co-authored-by: Nate Cottle <cottle.nate@gmail.com>
    Co-authored-by: Rita Bennett-Chew <ritabc@gmail.com>
    Co-authored-by: Reese Glasscock <reesegpdx@gmail.com>

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,11 +1,35 @@
 class DestinationsController < ApplicationController
   def index
     @destinations = Destination.all
-    json_response(@quotes)
+    json_response(@destinations)
+  end
+
+  def show
+    @destination = Destination.find(params[:id])
+    json_response(@destination)
+  end
+
+  def create
+    @destination = Destination.create!(destination_params)
+    json_response(@destination)
+  end
+
+  def update
+    @destination = Destination.find(params[:id])
+    @destination.update(destination_params)
+  end
+
+  def destroy
+    @destination = Destination.find(params[:id])
+    @destination.destroy
   end
 
   private
-  def json_response(object)
-    render json: object, status: :ok
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def destination_params
+    params.permit(:country, :city, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :destinations
+
+  resources :destinations do
+    resources :reviews
+  end
+
+  resources :activities
+
+  resources :users do
+    resources :reviews
+  end
+
+  resources :excursions ## require activity(s) and destination
 end
